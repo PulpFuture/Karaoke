8e36bae4e9e81d4092c6b2e36ddf7e222f425dde
Adam Shockley <pulpfuture@gmail.com>
2019-08-16 13:30:29 -0400

    init

---
+++ src/com/teamtreehouse/model/SongRequest.java
@@ -0,0 +1,12 @@
+package com.teamtreehouse.model;
+
+public class SongRequest {
+    private String mSingerName;
+    private Song mSong;
+
+    public SongRequest(String singerName, Song song) {
+        mSingerName = singerName;
+        mSong = song;
+    }
+}
+
